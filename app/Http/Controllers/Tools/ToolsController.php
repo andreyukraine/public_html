97a15c7131f511811d77b18a2502bb2aba0aec4b
ukr.web.ua <ukr.web.ua@gmail.com>
2018-12-03 23:42:46 +0200

    add api json partners

--- app/Http/Controllers/Tools/ToolsController.php
+++ app/Http/Controllers/Tools/ToolsController.php
@@ -31,9 +31,11 @@ class ToolsController extends Controller
 
     public function importJson(Request $request){
         $data = $request->json()->all();
-        if ($data['data']){
-            return "ok";
-        }
+//        if ($data['data']){
+//            return "ok";
+//        }else{
+//            return "not json data";
+//        }
         if ($data['key'] == "1234567890"){
             $users_sql = DB::table('partners')->where('index','=','1c')->get()->all();
             foreach ($users_sql as $user_sql){
