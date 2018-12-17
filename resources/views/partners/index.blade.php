f81cfe6d858abbb501c30b7e6bd59b44636db6ec
ukr.web.ua <ukr.web.ua@gmail.com>
2018-12-17 23:01:35 +0200

    add link shops

--- resources/views/partners/index.blade.php
+++ resources/views/partners/index.blade.php
@@ -92,7 +92,7 @@
                 <div class="tab-pane fade" id="nav-s" role="tabpanel" aria-labelledby="nav-profile-tab">
                     @foreach($ecommerces as $shope)
                         @foreach($shope as $item)
-                            <div class="adres" id="{{$item['id']}}"><?php echo mb_strimwidth($item['addres'], 0, 70, "...");?></div>
+                            <div class="adres" id="{{$item['id']}}"><a href="<?=$item['addres']?>"><?php echo mb_strimwidth($item['addres'], 0, 70, "...");?></a></div>
                         @endforeach
                     @endforeach
                 </div>
