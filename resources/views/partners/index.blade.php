6e9f4fb444d42aec0f06c0ed79e44f5e144c5e50
UA <ukr.web.ua@gmail.com>
2019-01-22 13:41:49 +0200

    fix

--- resources/views/partners/index.blade.php
+++ resources/views/partners/index.blade.php
@@ -68,7 +68,7 @@
             </div>
             <div class="tab-content" id="nav-tabContent">
                 <div class="tab-pane fade show active" id="nav-o" role="tabpanel" aria-labelledby="nav-home-tab">
-                        <div class="map-wrapper col-lg-12">
+                        <div class="map-wrapper col-lg-6">
                             <div id="map" style="width:100%;height:500px;">
                             </div>
                         </div>
