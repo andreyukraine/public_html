5533bb4c7c6ad8a330c604edc11487b18081a66f
UA <ukr.web.ua@gmail.com>
2019-01-24 02:50:12 +0200

    modal background

--- resources/lang/ua/catalog.php
+++ resources/lang/ua/catalog.php
@@ -1,6 +1,6 @@
 <?php
 return [
     'header_catalog' => 'КАТАЛОГ ТОВАРІВ СHICOPEE',
-    'header_title' => 'Products for dogs - Chicopee.in.ua | Buy Products for dogs in Kiev: price, reviews, sale ☎ +38 050 1 907 177',
-    'header_desc' => 'Products for dogs - Chicopee.in.ua | Buy Products for dogs in Kiev: price, reviews, sale',
+    'header_title' => 'Товары для собак і котів',
+    'header_desc' => 'Товары для собак і котів | Купити Товари для собак і котів в Києві',
 ];
--- resources/views/partners/index.blade.php
+++ resources/views/partners/index.blade.php
@@ -82,7 +82,7 @@
                                                 <div class="region">{{$key}}</div>
                                             @foreach($shop as $item)
                                                     {{--<div class="city">{{$item['name']}}</div>--}}
-                                                    <div class="adres" id="{{$item['id']}}"><?php echo mb_strimwidth($item['addres'], 0, 70, "...");?></div>
+                                                    <div class="adres" id="{{$item['id']}}"><?php echo mb_strimwidth($item['addres'], 0, 100, "...");?></div>
                                                 @endforeach
                                         @endforeach
                                     </div>
@@ -94,7 +94,7 @@
                 <div class="tab-pane fade" id="nav-s" role="tabpanel" aria-labelledby="nav-profile-tab">
                     @foreach($ecommerces as $shope)
                         @foreach($shope as $item)
-                            <div class="adres" id="{{$item['id']}}"><a target="_blank" href="<?=$item['url']?>"><?php echo mb_strimwidth($item['addres'], 0, 70, "...");?></a></div>
+                            <div class="adres" id="{{$item['id']}}"><a target="_blank" href="<?=$item['url']?>"><?php echo mb_strimwidth($item['addres'], 0, 150, "...");?></a></div>
                         @endforeach
                     @endforeach
                 </div>
